1bcc61dfb1b21555a02cf11063120e23aca6a160
marcos <marcos@gmail.com>
2024-10-21 08:22:08 -0300

    adicionado outro teste

--- test/example.test.js
+++ test/example.test.js
@@ -3,6 +3,6 @@ const assert = require('assert');
 
 describe('Exemplo de Teste', function() {
   it('deve somar dois números corretamente', function() {
-    assert.strictEqual(1 + 1, 2);
+    assert.strictEqual(1 + 11, 12);
   });
 });
